a23ef053c19bd9795117ca346c39b785498d374b
Christian <chrisi.schrefl@gmail.com>
2019-03-06 22:50:20 +0100

    First somewhot working version

---
+++ Backend.py
@@ -0,0 +1,46 @@
+#pip install wheel
+#pip install tornado
+
+#Run with Python 2.7
+
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+        clients.append(self)
+
+    def on_message(self, message):
+        for client in clients:
+            client.write_message(message)
+        
+        #self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+        clients.remove(self)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class GameHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("game.html")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/game", GameHandler),
+        (r"/ws", EchoWebSocket),
+    ])
+
+if __name__ == "__main__":
+    clients = []
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
